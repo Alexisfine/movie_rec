37aa826c9fb54ce63ab3ebd18f3d01496736c3c3
Alexisfine <89954228+Alexisfine@users.noreply.github.com>
2024-03-17 12:07:55 +0800

    added google search handler (#3)

--- hello.go
+++ hello.go
@@ -1,6 +1,26 @@
 package main
 import "fmt"
+import "net/http"
+import "io"
+import "strings"
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/google_search", func(w http.ResponseWriter, r *http.Request) {
+        resp, err := http.Get("https://www.google.com")
+		defer resp.Body.Close()
+		if err != nil {
+			fmt.Println("error in requesting google")
+			fmt.Fprintf(w, "failed to fetch request from google")
+			return 
+		}
+
+		buf := new(strings.Builder)
+		_, err = io.Copy(buf, resp.Body)
+		fmt.Fprintf(w, buf.String())
+    })
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello")
+    })
+    http.ListenAndServe(":80", nil)
 }
